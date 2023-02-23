122ebc1abe5576931877981f488e94f990b2672b
Adam Warren <113055287+1031warren@users.noreply.github.com>
2023-02-23 15:33:21 -0700

    Add files via upload

---
+++ w7.js
@@ -0,0 +1,10 @@
+
+function processForm(){
+    document.getElementById("userInfo").style.display ; 
+    document.getElementById("Uname2").innerText = document.getElementById("Uname").value;
+    document.getElementById("Weapons2").innerText = document.getElementById("Weapons").value;
+    document.getElementById("Health/Damage2").innerText = document.getElementById("Health/Damage").value;
+    document.getElementById("Points2").innerText = document.getElementById("Points").value;
+    
+
+}
